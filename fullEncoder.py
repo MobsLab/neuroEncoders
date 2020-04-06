7c82abfed18e0b780a3e9d57af79656244508000
twiby <t.balenbois@gmail.com>
2020-04-06 18:43:34 +0200

    works but so slow

--- fullEncoder.py
+++ fullEncoder.py
@@ -108,7 +108,7 @@ class Params:
 	def __init__(self):
 		self.nGroups = np.max(GRP_train) + 1
 		self.dim_output = POS_train.shape[1]
-		self.nChannels = SPK_train[0].shape[0]
+		self.nChannels = spikeDetector.numChannelsPerGroup()
 		self.length = len(SPK_train)
 
 		self.nSteps = 10000
@@ -238,6 +238,20 @@ class spikeNet:
 
 
 
+
+
+# temp = []
+# for g in range(params.nGroups):
+# 	temp.append(SPK_train.copy())
+# 	for spk in range(len(SPK_train)):
+# 		if GRP_train[spk]!=g:
+# 			temp[g][spk] = np.zeros([params.nChannels[g], 32])
+# 	temp[g] = np.stack(temp[g], axis=0)
+
+
+
+
+
 def getTrainingSpikesWithBlanks():
 	totalLength = SPT_train[-1] - SPT_train[0]
 	nBins = int(totalLength // params.windowLength) - 1
@@ -248,36 +262,19 @@ def getTrainingSpikesWithBlanks():
 		for binStart in binStartTime:
 			allSpikes=[]
 			sel = np.logical_and(SPT_train>binStart, SPT_train<binStart+params.windowLength)
-			length = np.array([np.sum(sel)])
+			length = np.sum(sel)
 			for group in range(params.nGroups):
 				spikes = SPK_train[sel]
 				groups = GRP_train[sel]==group
 				for spk in range(spikes.shape[0]):
-					spikes[spk,:,:] *= groups[spk]
+					if not groups[spk]:
+						spikes[spk] = np.zeros([params.nChannels[group], 32])
 
-				spikes = np.pad(spikes, ((0, params.maxLength-spikes.shape[0]),(0,0),(0,0)), 'constant', constant_values=0)
+				spikes = np.pad(np.stack(spikes, axis=0), ((0, params.maxLength-spikes.shape[0]),(0,0),(0,0)), 'constant', constant_values=0)
 				allSpikes.append(spikes)
 			pos = POS_train[SPT_train>binStart][0,:]
-			yield allSpikes, pos.reshape([1,2])/np.max(POS_train), length
+			yield tuple(allSpikes) + (pos/np.max(POS_train), length)
 
-def batch(gen):
-	while True:
-		binSpk = []
-		binPos = []
-		binLen = []
-		for _ in range(params.batch_size):
-			spk, pos, length = next(gen)
-			binSpk.append(spk)
-			binPos.append(pos)
-			binLen.append(length)
-		spk = np.stack(binSpk, axis=0)
-		pos = np.stack(binPos, axis=0)
-		length = np.stack(binLen, axis=0)
-		print(spk.shape)
-		print(pos.shape)
-		print(length.shape)
-		bbbb
-		yield spk, pos, length
 
 
 ### Training model
@@ -286,7 +283,16 @@ with tf.Graph().as_default():
 	print()
 	print('TRAINING')
 
-	# dataIterator, trainingTensors, dataPlaceholders = datasetMaker.makeDataset(params, training=True)
+	dataset = tf.data.Dataset.from_generator(
+		getTrainingSpikesWithBlanks, 
+		output_types = tuple(tf.float32 for _ in range(params.nGroups)) + (tf.float64, tf.int64),
+		output_shapes = tuple(tf.TensorShape([params.maxLength, params.nChannels[g], 32]) for g in range(params.nGroups)) + (tf.TensorShape([2]), tf.TensorShape([])))
+	dataset = dataset.batch(params.batch_size)
+	iter = dataset.make_initializable_iterator()
+	el = iter.get_next()
+	spikesPH, posPH, lengthPH =  el[:params.nGroups], el[params.nGroups], el[params.nGroups+1]
+
+
 	with tf.device(device_name):
 		spkParserNet = []
 		allFeatures = []
@@ -294,23 +300,22 @@ with tf.Graph().as_default():
 		# CNN plus dense on every group indepedently
 		for group in range(params.nGroups):
 			with tf.variable_scope("group"+str(group)+"-encoder"):
-				x = tf.placeholder(tf.float32, shape=[params.maxLength, params.batch_size, params.nChannels, 32], name="x")
-				x = x.reshape([-1, params.nChannels[group], 32])
+				x = tf.transpose(spikesPH[group], [1,0,2,3])
+				x = tf.reshape(x, [-1, params.nChannels[group], 32])
 				realSpikes = tf.math.logical_not(tf.equal(tf.reduce_sum(x, [1,2]), tf.constant(0.)))
 				nSpikesTot = tf.shape(x)[0]; idMatrix = tf.eye(nSpikesTot)
 				completionTensor = tf.transpose(tf.gather(idMatrix, tf.where(realSpikes))[:,0,:], [1,0], name="completion")
 				x = tf.boolean_mask(x, realSpikes)
 
-			newSpikeNet = spikeNet(nChannels=params.nChannels, device="/cpu:0", nFeatures=params.nFeatures)
+			newSpikeNet = spikeNet(nChannels=params.nChannels[group], device="/cpu:0", nFeatures=params.nFeatures)
 			x = newSpikeNet.apply(x)
 			x = tf.matmul(completionTensor, x)
+			x = tf.reshape(x, [params.maxLength, params.batch_size, params.nFeatures])
 			allFeatures.append(x)
 		allFeatures = tf.tuple(allFeatures)
-		allFeatures = tf.concat(allFeatures, axis=1)
-		# allFeatures = tf.concat(allFeatures, axis=2)
+		allFeatures = tf.concat(allFeatures, axis=2)
 
 		# LSTM on the concatenated outputs of previous graphs
-		lengthPH = tf.placeholder(tf.int32, shape=[None], name="length")
 		if device_name=="/gpu:0":
 			lstm = tf.contrib.cudnn_rnn.CudnnLSTM(params.lstmLayers, params.lstmSize, dropout=params.lstmDropout)
 			outputs, finalState = lstm(allFeatures, training=True)
@@ -319,23 +324,20 @@ with tf.Graph().as_default():
 			lstm = tf.nn.rnn_cell.MultiRNNCell(lstm)
 			outputs, finalState = tf.nn.dynamic_rnn(
 				lstm, 
-				tf.expand_dims(allFeatures, axis=1), 
+				allFeatures, 
 				dtype=tf.float32, 
 				time_major=params.timeMajor,
 				sequence_length=lengthPH)
-				# sequence_length=trainingTensors[1])
 
 	# dense to extract regression on output and loss
 	denseOutput = tf.layers.Dense(params.dim_output, activation = None, name="pos")
 	denseLoss1  = tf.layers.Dense(params.lstmSize, activation = tf.nn.relu, name="loss1")
 	denseLoss2  = tf.layers.Dense(1, activation = params.lossActivation, name="loss2")
 
-	# output = last_relevant(outputs, trainingTensors[1], timeMajor=params.timeMajor)
 	output = last_relevant(outputs, lengthPH, timeMajor=params.timeMajor)
 	outputLoss = denseLoss2(denseLoss1(output))[:,0]
 	outputPos = denseOutput(output)
 
-	posPH = tf.placeholder(tf.float32, shape=[None, 2], name="pos")
 	lossPos =  tf.losses.mean_squared_error(outputPos, posPH, reduction=tf.losses.Reduction.NONE)
 	lossPos =  tf.reduce_mean(lossPos, axis=1)
 	lossLoss = tf.losses.mean_squared_error(outputLoss, lossPos)
@@ -350,16 +352,11 @@ with tf.Graph().as_default():
 
 	### Training and testing
 	trainLosses = []
-	batchGen = getTrainingSpikesWithBlanks()
 	with tf.Session() as sess:
 
 		# initialize variables and input framework
 		sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
-		# sess.run(dataIterator.initializer, 
-		# 	feed_dict={dataPlaceholders['groups']:GRP_train, 
-		# 	dataPlaceholders['timeStamps']:SPT_train, 
-		# 	dataPlaceholders['spikes']:SPK_train, 
-		# 	dataPlaceholders['positions']:POS_train / np.max(POS_train)})
+		sess.run(iter.initializer)
 
 		### training
 		epoch_loss = 0
@@ -370,12 +367,7 @@ with tf.Graph().as_default():
 
 			for lr in range(len(params.learningRates)):
 				if (i < (lr+1) * params.nSteps / len(params.learningRates)) and (i >= lr * params.nSteps / len(params.learningRates)):
-					spk, position, length = next(batchGen)
-					fDct = {tf.get_default_graph().get_tensor_by_name("group"+str(group)+"-encoder/x:0"):spk[group] for group in range(params.nGroups)}
-					fDct.update({posPH: position, lengthPH: length})
-					_, c, c2 = sess.run(
-						[optimizers[lr], lossPos, lossLoss], 
-						feed_dict=fDct)
+					_, c, c2 = sess.run([optimizers[lr], lossPos, lossLoss])
 					break
 				if lr==len(params.learningRates)-1:
 					print('not run:',i)
@@ -513,12 +505,12 @@ with tf.Graph().as_default(), tf.device("/cpu:0"):
 	embeddings = []
 	for group in range(params.nGroups):
 		with tf.variable_scope("group"+str(group)+"-encoder"):
-			x = tf.placeholder(tf.float32, shape=[None, params.nChannels, 32], name="x")
+			x = tf.placeholder(tf.float32, shape=[None, params.nChannels[group], 32], name="x")
 			realSpikes = tf.math.logical_not(tf.equal(tf.reduce_sum(x, [1,2]), tf.constant(0.)))
 			nSpikesTot = tf.shape(x)[0]; idMatrix = tf.eye(nSpikesTot)
 			completionTensor = tf.transpose(tf.gather(idMatrix, tf.where(realSpikes))[:,0,:], [1,0], name="completion")
 			x = tf.boolean_mask(x, realSpikes)
-		newSpikeNet = spikeNet(nChannels=params.nChannels, device="/cpu:0", nFeatures=params.nFeatures)
+		newSpikeNet = spikeNet(nChannels=params.nChannels[group], device="/cpu:0", nFeatures=params.nFeatures)
 		x = newSpikeNet.apply(x)
 		x = tf.matmul(completionTensor, x)
 		# x = tf.pad(x, [[0,0],[group*params.nFeatures, (params.nGroups-group-1)*params.nFeatures]], "CONSTANT")
