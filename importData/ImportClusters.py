58b030781c407910ded64fe2adff8826dc4d4dc2
bryzgalovdm <bryzgalovdm@gmail.com>
2021-03-23 18:32:13 +0100

    simplebayes v0.9 alpha

--- importData/ImportClusters.py
+++ importData/ImportClusters.py
@@ -3,6 +3,7 @@ import sys
 import os
 import tables
 import struct
+import pickle
 import numpy as np
 import xml.etree.ElementTree as ET
 from functools import reduce
@@ -26,14 +27,17 @@ def getBehavior(folder, bandwidth=None):
 	if bandwidth == None:
 		bandwidth = (np.max(positions) - np.min(positions))/20
 	learning_time = stop_time - start_time
-	
-	return {'Positions': positions, 'Position_time': position_time, 'Speed': speed, 'Times': {'start': start_time, 'stop': stop_time, 'end': end_time, 'learning': learning_time}}
+
+	behavior_data = {'Positions': positions, 'Position_time': position_time, 'Speed': speed, 'Bandwidth': bandwidth,
+		'Times': {'start': start_time, 'stop': stop_time, 'end': end_time, 'learning': learning_time}}
+
+	return behavior_data
 
 
-def getSpikesfromClu(projectPath, behavior_data, cluster_modifier=1):
+def getSpikesfromClu(projectPath, behavior_data, cluster_modifier=1, savedata=True):
 	# Get parameters
 	list_channels, samplingRate, _ = rawDataParser.get_params(projectPath.xml)
-	
+
 	# Allocate
 	labels = []
 	spike_time = []
@@ -41,40 +45,47 @@ def getSpikesfromClu(projectPath, behavior_data, cluster_modifier=1):
 	spike_speed = []
 
 	n_tetrodes = len(list_channels)
-	for tetrode in range(n_tetrodes+1):
+	for tetrode in range(n_tetrodes):
 		if os.path.isfile(projectPath.clu(tetrode)):
 			with open(
 					projectPath.clu(tetrode), 'r') as fClu, open(
-					projectPath.basename + 'res.' + str(tetrode), 'r') as fRes, open(
-					projectPath.basename + 'spk.' + str(tetrode), 'rb') as fSpk:
+					projectPath.res(tetrode), 'r') as fRes, open(
+					projectPath.spk(tetrode), 'rb') as fSpk:
 				clu_str = fClu.readlines()
 				res_str = fRes.readlines()
 				n_clu = int(clu_str[0])-1
-				n_channels = len(list_channels[tetrode])
-				spikeReader = struct.iter_unpack(str(32*n_channels)+'h', fSpk.read())
+				# n_channels = len(list_channels[tetrode])
+				# spikeReader = struct.iter_unpack(str(32*n_channels)+'h', fSpk.read())
 
 				# labels = np.array([[1. if int(clu_str[n+1])-1==l else 0. for l in range(n_clu)] for n in range(len(clu_str)-1) if (int(clu_str[n+1])!=0)])
 				# spike_time = np.array([[float(res_str[n])/samplingRate] for n in range(len(clu_str)-1) if (int(clu_str[n+1])!=0)])
-				labels = np.array([[1. if int(clu_str[n+1])==l else 0. for l in range(n_clu+1)] for n in range(len(clu_str)-1)])
-				spike_time.append(np.array([[float(res_str[n])/samplingRate] for n in range(len(clu_str)-1)]))
-				spike_positions.append(np.array([behavior_data['Positions'][np.argmin(np.abs(spike_time[n]-behavior_data['Position_time'])),:] for n in range(len(spike_time))]))
-				spike_speed.append(np.array([behavior_data['Speed'][np.min((np.argmin(np.abs(spike_time[n]-behavior_data['Position_time'])),
-					len(behavior_data['Speed'])-1)),:] for n in range(len(spike_time))]))
+				labels_temp = butils.modify_labels(np.array([[1. if int(clu_str[n+1])==l else 0. for l in range(n_clu+1)] for n in range(len(clu_str)-1)]), cluster_modifier)
+				st = (np.array([[float(res_str[n])/samplingRate] for n in range(len(clu_str)-1)]))
+				sp = (np.array([behavior_data['Positions'][np.argmin(np.abs(st[n]-behavior_data['Position_time'])),:] for n in range(len(st))]))
+				ss = (np.array([behavior_data['Speed'][np.min((np.argmin(np.abs(st[n]-behavior_data['Position_time'])),
+					len(behavior_data['Speed'])-1)),:] for n in range(len(st))]))
+
+				spike_time.append(st)
+				spike_positions.append(sp)
+				spike_speed.append(ss)
+				labels.append(labels_temp)
 		else:
-			print("File "+ projectPath.clu(tetrode) + 'clu.' + str(tetrode) +" not found.")
+			print("File "+ projectPath.clu(tetrode) +" not found.")
 		continue
-		sys.stdout.write('File from tetrode ' + str(tetrode) + ' has been succesfully opened. ')
+		sys.stdout.write('File from tetrode '+ ' has been successfully opened. ')
 		sys.stdout.write('Processing ...')
 		sys.stdout.write('\r')
 		sys.stdout.flush()
-		
-		labels.append(butils.modify_labels(labels, cluster_modifier))
-		
-		sys.stdout.write('We have finished building rates for group ' + str(tetrode) + ', loading next                           ')
+
+		sys.stdout.write('We have finished building rates for group ' + str(tetrode+1) + ', loading next                           ')
 		sys.stdout.write('\r')
 		sys.stdout.flush()
 	sys.stdout.write('We have finished building rates.                                                           ')
 	sys.stdout.write('\r')
 	sys.stdout.flush()
- 
-	return labels, spike_time, spike_positions, spike_time
+
+	cluster_data = {'Spike_labels': labels, 'Spike_times': spike_time, 'Spike_positions': spike_positions, 'Spike_speed': spike_speed}
+	if savedata:
+		np.save(projectPath.folder + 'ClusterData.npy', cluster_data)
+
+	return cluster_data
