7911e72fb83ead214c4cde576532f8b9eff1dc10
bryzgalovdm <bryzgalovdm@gmail.com>
2021-03-26 21:14:04 +0100

    bayes decoding beta v1

--- SimpleBayes/decodebayes.py
+++ SimpleBayes/decodebayes.py
@@ -41,19 +41,19 @@ class Trainer():
 		Occupation_inverse = np.multiply(Occupation_inverse, mask)
 
 		### Built spike-related kernels
-		fake_labels_time = []
-		fake_labels = []
+		guessed_clusters_time = []
+		guessed_clusters = []
 		Marginal_rate_functions = []
 		Local_rate_functions = []
 		Rate_functions = []
 
 		n_tetrodes = len(cluster_data['Spike_labels'])
 		for tetrode in range(n_tetrodes):
-			fake_labels_time.append(cluster_data['Spike_times'][tetrode][reduce(np.intersect1d,
+			guessed_clusters_time.append(cluster_data['Spike_times'][tetrode][reduce(np.intersect1d,
 				(np.where(cluster_data['Spike_times'][tetrode][:,0] > behavior_data['Times']['stop']),
 				np.where(cluster_data['Spike_times'][tetrode][:,0] < behavior_data['Times']['end']),
 				np.where(cluster_data['Spike_times'][tetrode][:,0] > speed_cut)))])
-			fake_labels.append(butils.shuffle_labels(cluster_data['Spike_labels'][tetrode][reduce(np.intersect1d,
+			guessed_clusters.append(butils.shuffle_labels(cluster_data['Spike_labels'][tetrode][reduce(np.intersect1d,
 				(np.where(cluster_data['Spike_times'][tetrode][:,0] > behavior_data['Times']['stop']),
 				np.where(cluster_data['Spike_times'][tetrode][:,0] < behavior_data['Times']['end']),
 				np.where(cluster_data['Spike_times'][tetrode][:,0] > speed_cut)))], rand_param))
@@ -88,35 +88,29 @@ class Trainer():
 			Rate_functions.append(Local_rate_functions)
 
 		bayes_matrices = {'Occupation': Occupation, 'Marginal rate functions': Marginal_rate_functions, 'Rate functions': Rate_functions,
-				'Bins':[xEdges[:,0],yEdges[0,:]],'fake_labels_info': {'clusters':fake_labels, 'time':fake_labels_time},
-				'time_limits': [behavior_data['Times']['start'], behavior_data['Times']['stop']]}
+				'Bins':[xEdges[:,0],yEdges[0,:]],'guessed_clusters_info': {'clusters':guessed_clusters, 'time':guessed_clusters_time},
+				'time_limits': [behavior_data['Times']['start'], behavior_data['Times']['stop'], behavior_data['Times']['end']]}
 
 		return bayes_matrices
 
 
 
-	def test(self, project_path, bayes_matrices, behavior_data, windowSize=0.036, speed_cut=0, masking_factor=20):
+	def test(self, bayes_matrices, behavior_data, windowSize=0.036, masking_factor=20):
 
 		print('\nBUILDING POSITION PROBAS')
 
 		### Format matrices
-		guessed_clusters_info = bayes_matrices['fake_labels_info']
-		space_bins = [np.array((bayes_matrices['Bins'][0][b],bayes_matrices['Bins'][1][b])) for b in range(np.shape(bayes_matrices['Bins'][0])[0])]
+		guessed_clusters_info = bayes_matrices['guessed_clusters_info']
 		Occupation, Marginal_rate_functions, Rate_functions = [bayes_matrices[key] for key in ['Occupation','Marginal rate functions','Rate functions']]
 		guessed_clusters, guessed_clusters_time = [guessed_clusters_info[key] for key in ['clusters','time']]
 		mask = Occupation > (np.max(Occupation)/masking_factor)
 
-		# for tetrode in range(np.shape(guessed_clusters)[0]):
-		# 	for spike in range(np.shape(guessed_clusters[tetrode])[0]):
-		# 		if guessed_clusters[tetrode][spike, 0] > 1/np.shape(guessed_clusters[tetrode])[1]:
-		# 			guessed_clusters[tetrode][spike, :] = np.zeros([1,np.shape(guessed_clusters[tetrode])[1]])
-		# 		else:
-		# 			guessed_clusters[tetrode][spike, 0] = 0
-		# 			guessed_clusters[tetrode][spike, :] = guessed_clusters[tetrode][spike, :]/np.sum(guessed_clusters[tetrode][spike, :], 0)
+		# Constant term
+		ConstantTerm = np.sum(bayes_matrices['Marginal rate functions'], axis=0)
 
 		### Build Poisson term
-		n_bins = math.floor((bayes_matrices['time_limits'][1] - bayes_matrices['time_limits'][0])/windowSize)
-		All_Poisson_term = [np.exp( (-windowSize)*Marginal_rate_functions[tetrode] ) for tetrode in range(len(guessed_clusters))]
+		n_bins = math.floor((behavior_data['Times']['end'] - behavior_data['Times']['stop'])/windowSize)
+		All_Poisson_term = [np.exp( (-windowSize)*Marginal_rate_functions[tetrode]) for tetrode in range(len(guessed_clusters))]
 		All_Poisson_term = reduce(np.multiply, All_Poisson_term)
 
 		### Log of rate functions
@@ -131,13 +125,16 @@ class Trainer():
 		position_proba = [np.ones(np.shape(Occupation))] * n_bins
 		position_true = [np.ones(2)] * n_bins
 		nSpikes = []
+		times = []
 		for bin in range(n_bins):
 
-			bin_start_time = behavior_data['Times']['start'] + bin*windowSize
+			bin_start_time = behavior_data['Times']['stop'] + bin*windowSize
 			bin_stop_time = bin_start_time + windowSize
+			times.append(bin_start_time)
 
 			binSpikes = 0
 			tetrodes_contributions = []
+			tetrodes_contributions.append(ConstantTerm)
 			tetrodes_contributions.append(All_Poisson_term)
 
 			for tetrode in range(len(guessed_clusters)):
@@ -152,25 +149,26 @@ class Trainer():
 				# Terms that come from spike information (with normalization)
 				if np.sum(bin_clusters) > 0.5:
 					place_maps = reduce(np.multiply,
-						[butils.exp256( (log_RF[tetrode][cluster] * bin_clusters[cluster]) )
+						[np.power(log_RF[tetrode][cluster], np.ones(np.shape(Occupation)) * bin_clusters[cluster])
 						for cluster in range(np.shape(bin_clusters)[0])])
 
 
-					spike_pattern = math.pow(windowSize, np.sum(bin_clusters)) * place_maps
+					spike_pattern = place_maps
 				else:
 					spike_pattern = np.multiply(np.ones(np.shape(Occupation)), mask)
 
-				tetrodes_contributions.append( spike_pattern )#/np.sum(spike_pattern) )
+				tetrodes_contributions.append(spike_pattern)
 
 			nSpikes.append(binSpikes)
 
-			position_proba[bin] = reduce( np.multiply, tetrodes_contributions )
+			AllSpikesContribution = reduce(np.multiply, tetrodes_contributions)
+			position_proba[bin] = np.multiply(AllSpikesContribution, Occupation)
+			# Guessed probability map
 			position_proba[bin] = position_proba[bin] / np.sum(position_proba[bin])
-
+			# True position
 			position_true_mean = np.nanmean( behavior_data['Positions'][reduce(np.intersect1d,
 				(np.where(behavior_data['Position_time'][:,0] > bin_start_time),
-				np.where(behavior_data['Position_time'][:,0] < bin_stop_time),
-				np.where(behavior_data['Speed'][:,0] > speed_cut)))], axis=0 )
+				np.where(behavior_data['Position_time'][:,0] < bin_stop_time)))], axis=0 )
 			position_true[bin] = position_true[bin-1] if np.isnan(position_true_mean).any() else position_true_mean
 
 			if bin % 50 == 0:
@@ -183,7 +181,17 @@ class Trainer():
 
 		position_true[0] = position_true[1]
 		print('\nDecoding finished')
-		return position_proba, position_true, nSpikes
+
+		# Guessed X and Y
+		xProba  = [np.sum(position_proba[bin], axis=1) for bin in range(len(nSpikes))]
+		xGuessed = [bayes_matrices['Bins'][0][np.argmax(xProba[bin])] for bin in range(len(nSpikes))]
+		yProba  = [np.sum(position_proba[bin], axis=0) for bin in range(len(nSpikes))]
+		yGuessed = [bayes_matrices['Bins'][1][np.argmax(yProba[bin])] for bin in range(len(nSpikes))]
+		bestProba = [np.max(position_proba[bin]) for bin in range(len(nSpikes))]
+		position_guessed = np.vstack((xGuessed, yGuessed, bestProba)).T
+
+		outputResults = {"inferring":position_guessed, "pos": np.array(position_true), "probaMaps": position_proba, "times":np.array(times), 'nSpikes': np.array(nSpikes)}
+		return outputResults
 
 
 
