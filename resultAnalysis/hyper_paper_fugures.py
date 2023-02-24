e49722a9d2cb776518d1bb04899b25174375cfde
bryzgalovdm <bryzgalovdm@gmail.com>
2023-02-24 11:16:28 +0100

    update requirements

--- resultAnalysis/hyper_paper_fugures.py
+++ resultAnalysis/hyper_paper_fugures.py
@@ -2,5 +2,6 @@ import numpy as np
 import seaborn as sns
 
 def barplot_linError(lErrorNN_mean, lErrorBayes_mean):
+    # TODO: trnsform data to pandas dataframe or other beautiful format for seaborn
     datToPlot = np.vstack((lErrorNN_mean, lErrorBayes_mean)).T
     sns.boxplot(data=datToPlot, x=['NN', 'Bayes'], y='absolute linear error')
