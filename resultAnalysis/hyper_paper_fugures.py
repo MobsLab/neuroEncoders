6127200beaae8e1d02f30ba77473897152a11389
bryzgalovdm <bryzgalovdm@gmail.com>
2023-04-14 13:21:04 +0200

    changed colors and added mouse-by-mouse accuracy in hyperfigures

--- resultAnalysis/hyper_paper_fugures.py
+++ resultAnalysis/hyper_paper_fugures.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 plt.style.use(os.path.join(os.getcwd(), 'resultAnalysis', 'ne.mplstyle'))
 import seaborn as sns
 
+cm = plt.get_cmap("tab20b")
+colorsForSNS = [cm(14), cm(2)]
+
 def boxplot_linError(lErrorNN_mean, lErrorBayes_mean, timeWindows=[36, 108, 252, 504],
                      dirSave=None, suffix=''):
     #TODO: add significance test?
@@ -12,10 +15,12 @@ def boxplot_linError(lErrorNN_mean, lErrorBayes_mean, timeWindows=[36, 108, 252,
     timeWindowsForDF = timeWindows * lErrorNN_mean.shape[0] * 2
     decoderForPD = ['ANN'] * lErrorNN_mean.shape[0] * len(timeWindows) + ['Bayes'] * lErrorNN_mean.shape[0] * len(timeWindows)
     datToPlot = pd.DataFrame({'linearError': data,
-                              'timeWindow': timeWindowsForDF,
+                              'timeWindow (ms)': timeWindowsForDF,
                               'decoder': decoderForPD})
     fig, ax = plt.subplots(figsize=(9, 9))
-    sns.boxplot(data=datToPlot, x="linearError", y="timeWindow", hue="decoder", orient='h', ax=ax)
+    myPalette = {"ANN": colorsForSNS[0], "Bayes": colorsForSNS[1]}
+    sns.boxplot(data=datToPlot, x="timeWindow (ms)", y="linearError",
+                hue="decoder", orient='v', ax=ax, palette=myPalette)
 
     if dirSave is not None:
         fig.savefig(os.path.join(dirSave, f'linearErrorBoxPlot{suffix}.png'))
@@ -27,11 +32,13 @@ def boxplot_euclError(errorNN_mean, errorBayes_mean, timeWindows=[36, 108, 252, 
     data = np.round(np.vstack((errorNN_mean, errorBayes_mean)).flatten(), 2)
     timeWindowsForDF = timeWindows * errorNN_mean.shape[0] * 2
     decoderForPD = ['ANN'] * errorNN_mean.shape[0] * len(timeWindows) + ['Bayes'] * errorNN_mean.shape[0] * len(timeWindows)
-    datToPlot = pd.DataFrame({'error (cm)': data,
+    datToPlot = pd.DataFrame({'eucl. error (cm)': data,
                               'timeWindow (ms)': timeWindowsForDF,
                               'decoder': decoderForPD})
     fig, ax = plt.subplots(figsize=(9, 9))
-    sns.boxplot(data=datToPlot, x="timeWindow (ms)", y="error (cm)", hue="decoder", orient='v', ax=ax)
+    myPalette = {"ANN": colorsForSNS[0], "Bayes": colorsForSNS[1]}
+    sns.boxplot(data=datToPlot, x="timeWindow (ms)", y="eucl. error (cm)", hue="decoder",
+                orient='v', ax=ax, palette=myPalette)
 
     if dirSave is not None:
         fig.savefig(os.path.join(dirSave, f'errorBoxPlot{suffix}.png'))
@@ -51,9 +58,32 @@ def barplot_linError_mouse_by_mouse(lErrorNN_mean, lErrorBayes_mean,
                                          'decoder': decoderForPD}))
 
         fig, ax = plt.subplots(figsize=(9, 9))
+        myPalette = {"ANN": colorsForSNS[0], "Bayes": colorsForSNS[1]}
         sns.barplot(data=dataByWindow[iWindow], x="mouseNames", y="linearError",
-                    hue="decoder", orient='v', ax=ax)
+                    hue="decoder", orient='v', ax=ax, palette=myPalette)
 
         if dirSave is not None:
             fig.savefig(os.path.join(dirSave, f'linearErrorBoxPlotMBM{suffix}_{timeWindows[iWindow]}.png'))
             fig.savefig(os.path.join(dirSave, f'linearErrorBoxPlotMBM{suffix}_{timeWindows[iWindow]}.svg'))
+
+def barplot_euclError_mouse_by_mouse(errorNN_mean, errorBayes_mean,
+                                    timeWindows=[36, 108, 252, 504],
+                                    mouseNames=['994', '1199_1', '1199_2', '1223'],
+                                    dirSave=None, suffix=''):
+    dataByWindow = []
+    for iWindow in range(len(timeWindows)):
+        data = np.round(np.vstack((errorNN_mean[:, iWindow], errorBayes_mean[:, iWindow])).flatten(), 2)
+        mouseNamesForDF = mouseNames * 2
+        decoderForPD = ['ANN'] * errorNN_mean.shape[0] + ['Bayes'] * errorBayes_mean.shape[0]
+        dataByWindow.append(pd.DataFrame({'eucl. error (cm)': data,
+                                          'mouseNames': mouseNamesForDF,
+                                          'decoder': decoderForPD}))
+
+        fig, ax = plt.subplots(figsize=(9, 9))
+        myPalette = {"ANN": colorsForSNS[0], "Bayes": colorsForSNS[1]}
+        sns.barplot(data=dataByWindow[iWindow], x="mouseNames", y="eucl. error (cm)",
+                    hue="decoder", orient='v', ax=ax, palette=myPalette)
+
+        if dirSave is not None:
+            fig.savefig(os.path.join(dirSave, f'errorBoxPlotMBM{suffix}_{timeWindows[iWindow]}.png'))
+            fig.savefig(os.path.join(dirSave, f'errorBoxPlotMBM{suffix}_{timeWindows[iWindow]}.svg'))
