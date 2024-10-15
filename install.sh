d300349f87626b142dabb939c80b9a15dbb74fe5
Théotime de Charrin <theotime.decharrin@gmail.com>
2024-10-15 16:34:58 +0200

    update README and installer - new for theotime

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 sudo apt-get install python3-dev python3-pip python3-venv python3-tk jq xmlstarlet
-python3 -m venv env
-env/bin/pip3 install -r requirements.txt
+if { conda env list | grep 'base'; } >/dev/null 2>&1; then
+    echo "Conda base environment exists, creating new environment for neuroEncoder"
+    conda env create -f environment.yml
+else
+    echo "Conda base environment does not exist"
+    echo "Please install conda"
+    exit 1
+fi
