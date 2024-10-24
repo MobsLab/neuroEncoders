4c2939efc057e479600c3c08198a9c4bf501e78c
Théotime de Charrin <theotime.de-charrin@espci.fr>
2024-10-24 15:32:44 +0200

    change paths

--- test.py
+++ test.py
@@ -7,16 +7,17 @@
 import os
 import subprocess
 
-# Load standard libs
-import sys
-
 from importData import rawdata_parser
 
-# Load custum code
+# Load custom code
 from utils.global_classes import Params, Project
 
+# Load standard libs
+
+
 # FILENAME = "/media/mickey/DimaERC2/TEST1_Basile/M1199_20210408_UMaze.xml"
-FILENAME = "/media/mickey/DimaERC2/TEST3_Basile_M1239/M1239_20211105_StimMFBWake.xml"
+# FILENAME = "/media/mickey/DimaERC2/TEST3_Basile_M1239/M1239_20211105_StimMFBWake.xml"
+FILENAME = os.path.expanduser("~/Documents/Theotime/M1199/M1199_20210408_UMaze.xml")
 nameExp = "TESTTHEOTIME"
 ProjectPath = Project(os.path.expanduser(FILENAME), nameExp=nameExp)
 folderCode = "/home/mickey/Documents/Theotime/neuroEncoders-master"
