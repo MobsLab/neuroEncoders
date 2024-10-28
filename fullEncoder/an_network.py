MobsLab/neuroEncoders

commit d4de9f80194351245253f3c846e7677ae213b46e

add multi GPU support through MirroredStrategy

fullEncoder/an_network.py
@@ -67,7 +67,18 @@ class LSTMandSpikeNetwork:
         self.trainLosses = {}
 
         ### Description of layers here
-        with tf.device(self.deviceName):
+
+        # Handle multiple GPUs - single host setup
+        self.strategy = tf.distribute.MirroredStrategy(
+            devices=(
+                [dev.replace("/device:", "") for dev in self.deviceName]
+                if isinstance(self.deviceName, list)
+                else [self.deviceName.replace("/device:", "")]
+            )
+        )
+        with self.strategy.scope():
+            # Everything that creates variables should be under the strategy scope.
+            # In general this is only model construction & `compile()`.
             if self.params.usingMixedPrecision:
                 self.inputsToSpikeNets = [
                     tf.keras.layers.Input(
@@ -164,19 +175,13 @@ class LSTMandSpikeNetwork:
 
             # Gather the full model
             outputs = self.generate_model()
-            # Handle multiple GPUs - single host setup
-            strategy = tf.distribute.MirroredStrategy()
-            print("Number of devices: {}".format(strategy.num_replicas_in_sync))
             # Build two models
             # One just desctibed, with two oibjective funcitons corresponding
             # to both position and predicted losses
-            with strategy.scope():
-                # Everything that creates variables should be under the strategy scope.
-                # In general this is only model construction & `compile()`.
-                self.model = self.compile_model(outputs)
-                # In theory, the predicted loss could be not learning enough in the first network (optional)
-                # Second only with loss corresponding to predicted loss
-                self.predLossModel = self.compile_model(outputs, predLossOnly=True)
+            self.model = self.compile_model(outputs)
+            # In theory, the predicted loss could be not learning enough in the first network (optional)
+            # Second only with loss corresponding to predicted loss
+            self.predLossModel = self.compile_model(outputs, predLossOnly=True)
 
     def get_theweights(self, behaviorData, windowsizeMS, isPredLoss=0):
         print("Loading the weights of the trained network")
@@ -200,7 +205,7 @@ class LSTMandSpikeNetwork:
 
     def generate_model(self):
         # CNN plus dense on every group independently
-        with tf.device(self.deviceName):
+        with self.strategy.scope():
             allFeatures = []  # store the result of the CNN computation for each group
             for group in range(self.params.nGroups):
                 x = self.inputsToSpikeNets[
@@ -352,7 +357,7 @@ class LSTMandSpikeNetwork:
 
     def generate_model_Cplusplus(self):
         ### Describe
-        with tf.device(self.deviceName):
+        with self.strategy.scope():
             allFeatures = []
             for group in range(self.params.nGroups):
                 x = self.inputsToSpikeNets[group]
@@ -1814,7 +1819,15 @@ class LSTMandSpikeNetwork_control:
         self.trainLosses = {}
 
         ### Description of layers here
-        with tf.device(self.deviceName):
+        # Handle multiple GPUs - single host setup
+        self.strategy = tf.distribute.MirroredStrategy(
+            devices=(
+                [dev.replace("/device:", "") for dev in self.deviceName]
+                if isinstance(self.deviceName, list)
+                else [self.deviceName.replace("/device:", "")]
+            )
+        )
+        with self.strategy.scope():
             if self.params.usingMixedPrecision:
                 self.inputsToSpikeNets = [
                     tf.keras.layers.Input(
@@ -1921,7 +1934,7 @@ class LSTMandSpikeNetwork_control:
 
     def generate_model(self):
         # CNN plus dense on every group independently
-        with tf.device(self.deviceName):
+        with self.strategy.scope():
             allFeatures = []  # store the result of the CNN computation for each group
             for group in range(self.params.nGroups):
                 x = self.inputsToSpikeNets[
@@ -2073,7 +2086,7 @@ class LSTMandSpikeNetwork_control:
 
     def generate_model_Cplusplus(self):
         ### Describe
-        with tf.device(self.deviceName):
+        with self.strategy.scope():
             allFeatures = []
             for group in range(self.params.nGroups):
                 x = self.inputsToSpikeNets[group]
@@ -2942,4 +2955,3 @@ class LSTMandSpikeNetwork_control:
 
 
 ########### HELPING LSTMandSpikeNetwork FUNCTIONS#####################
-

utils/management.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 
 def manage_devices(usedDevice):
+    # TODO: let the user chose 1 GPU or all GPUs
     # if gpu set memory growth
     if usedDevice == "GPU":
         device_phys = tf.config.list_physical_devices(usedDevice)
@@ -10,7 +11,9 @@ def manage_devices(usedDevice):
     # get the name of the device
     device = tf.config.list_logical_devices(usedDevice)
     if device:
-        devicename = device[0].name
+        devicename = [dv.name for dv in device]
+        if len(devicename) == 1:
+            devicename = devicename[0]
     else:
         devicename = device = tf.config.list_logical_devices()[0].name
 
@@ -19,4 +22,3 @@ def manage_devices(usedDevice):
 
 def convert_normalized_to_euclidian(normalized, maxPos):
     return normalized * maxPos
-

commit 233441b05d508335644a8520ec9201f68a86f306

fix: lambda issues (not globals) - probably caused by MirroredStrategy from d4de9f8 (add multi GPU support through MirroredStrategy, 2024-10-28). fixed by defining global function instead. lambda should not be defined on multiple lines.
update: README for better readability.

see https://github.com/tensorflow/tensorflow/issues/39832
https://stackoverflow.com/questions/62931610/warningtensorflowautograph-could-not-transform-function-format-example-at

fullEncoder/an_network.py
@@ -270,7 +270,7 @@ class LSTMandSpikeNetwork:
                     outputSeq = self.dropoutLayer(outputSeq)
             ### Outputs
             myoutputPos = self.denseFeatureOutput(output)  # positions
-            print("myuotputPos =", myoutputPos)
+            print("myoutputPos =", myoutputPos)
             outputPredLoss = self.denseLoss2(
                 self.denseLoss3(
                     self.denseLoss4(
@@ -492,10 +492,11 @@ class LSTMandSpikeNetwork:
                 ("dataset" + "_stride" + str(windowsizeMS) + ".tfrec"),
             )
         )
-        ndataset = ndataset.map(
-            lambda *vals: nnUtils.parse_serialized_spike(self.featDesc, *vals),
-            num_parallel_calls=tf.data.AUTOTUNE,
-        )
+
+        def _parse_function(*vals):
+            return nnUtils.parse_serialized_spike(self.featDesc, *vals)
+
+        ndataset = ndataset.map(_parse_function, num_parallel_calls=tf.data.AUTOTUNE)
         # Manage masks
         epochMask["train"] = inEpochsMask(
             behaviorData["positionTime"][:, 0], behaviorData["Times"]["trainEpochs"]
@@ -1999,7 +2000,7 @@ class LSTMandSpikeNetwork_control:
                     outputSeq = self.dropoutLayer(outputSeq)
             ### Outputs
             myoutputPos = self.denseFeatureOutput(output)  # positions
-            print("myuotputPos =", myoutputPos)
+            print("myoutputPos =", myoutputPos)
             outputPredLoss = self.denseLoss2(
                 self.denseLoss3(
                     self.denseLoss4(