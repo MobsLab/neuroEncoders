6907deb72c320ab33ed326389bd7ad756d92130a
Théotime de Charrin <theotime.decharrin@gmail.com>
2026-05-14 23:32:38 +0200

    update spectro for thierry

--- neuroencoders/utils/spectro_viewer.py
+++ neuroencoders/utils/spectro_viewer.py
@@ -1,10 +1,10 @@
 import argparse
 
-import neo
+import ephyviewer
+import spikeinterface.extractors as se
 from ephyviewer import (
-    AnalogSignalFromNeoRawIOSource,
     MainViewer,
-    SpectrogramViewer,
+    TimeFreqViewer,
     TraceViewer,
     mkQApp,
 )
@@ -14,37 +14,36 @@ argparser.add_argument("--filename", "-f", type=str, help="Path to Neuroscope da
 args = argparser.parse_args()
 # you must first create a main Qt application (for event loop)
 app = mkQApp()
-
-# create a fake signals 1 channel at 10kHz
-# this emulate a moving frequency
-
-
 # Create the main window that can contain several viewers
 win = MainViewer(debug=True, show_auto_scale=True)
 
-neorawio = neo.rawio.NeuroScopeRawIO(filename=args.filename)
+recording = se.extractor_classes.NeuroScopeRecordingExtractor(file_path=args.filename)
 
-source = AnalogSignalFromNeoRawIOSource(neorawio=neorawio)
+sig_source = ephyviewer.SpikeInterfaceRecordingSource(recording=recording)
 
 # create a viewer for signal with TraceViewer
-view1 = TraceViewer(source=source, name="trace")
+view1 = TraceViewer(source=sig_source, name="signals")
 view1.params["scale_mode"] = "same_for_all"
 view1.params["xsize"] = 5.0
 view1.auto_scale()
 
-
-# create a SpectrogramViewer on the same source
-view2 = SpectrogramViewer(source=source, name="spectrogram")
+# create a time freq viewer connected to the same source
+view2 = TimeFreqViewer(source=sig_source, name="tfr")
 
 view2.params["xsize"] = 5.0
-view2.params["colormap"] = "inferno"
-view2.params["scalogram", "binsize"] = 0.1
-view2.params["scalogram", "scale"] = "dB"
-view2.params["scalogram", "scaling"] = "spectrum"
+view2.params["show_axis"] = True
+view2.params["timefreq", "deltafreq"] = 1
+view2.params["timefreq", "f_start"] = 90
+view2.params["timefreq", "f_stop"] = 250
+view2.params["timefreq", "f0"] = 1
+view2.params["timefreq", "normalisation"] = 0
+view2.auto_scale()
 
 # add them to mainwindow
 win.add_view(view1)
 win.add_view(view2)
+view1.auto_scale()
+view2.auto_scale()
 
 
 # show main window and run Qapp
